c4664141802d631dcda00b58601cf80fc33e96ed
Patrice Thivierge F <pthivierge@gmail.com>
2015-09-04 13:34:03 +0200

    Added new configuration class to contain parameters globally in the application

---
+++ ApplicationCore/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalysesEngine.Core
+{
+    public static class Configuration
+    {
+        public static bool EnableAnalysisErrorOutput { get; set; }
+    }
+}
